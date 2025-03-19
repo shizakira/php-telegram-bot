c646f2be1c90b00de50f7c50cc386eb89f610afe
shizakira <ikirachn@gmail.com>
2025-03-20 04:33:29 +0500

    surround the head code with try catch

--- src/bin/bot.php
+++ src/bin/bot.php
@@ -1,9 +1,26 @@
 <?php
 
+declare(strict_types=1);
+
 require __DIR__.'/../vendor/autoload.php';
 
-use App\Services\TelegramBot;
+use App\Database\Repositories\UserRepository;
+use App\Services\TelegramBalanceService;
+use App\TelegramBot;
+use App\Database\Connection;
+use Telegram\Bot\Api;
 
-$bot = new TelegramBot(getenv('TELEGRAM_TOKEN'));
-$bot->run();
+try {
+    $conn = new Connection(config('postgres_database'));
+    $userRepository = new UserRepository($conn->getConnection());
+    $telegramBalanceService = new TelegramBalanceService($userRepository);
+    $telegramApi = new Api(getenv('TELEGRAM_TOKEN'));
+    $bot = new TelegramBot($telegramApi, $telegramBalanceService);
+
+    $bot->run();
+} catch (Throwable $e) {
+    errorLog($e->getMessage());
+}
+
+
 
